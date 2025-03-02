7591a13b36cee54fa8e374969db44517ffe34239
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-03-02 12:24:18 +0100

    Add LODs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 use std::f64::consts::TAU;
 
-use num_traits::Float;
 use raylib::prelude::*;
 
-use num_complex::{c64, Complex, Complex64, ComplexFloat};
+use num_complex::{Complex64, ComplexFloat};
 
-fn fft_helper<T>(data: &Vec<Complex<T>>, sign: i32) -> Vec<Complex<T>>
-where
-    T: Copy + Float + Default + From<i32> + From<f64>,
-{
+fn fft_helper(data: &Vec<Complex64>, sign: i32) -> Vec<Complex64> {
     let n = data.len();
     assert_eq!(n.count_ones(), 1);
 
@@ -16,18 +12,16 @@ where
         return data.clone();
     }
 
-    let evens: Vec<Complex<T>> = data.iter().step_by(2).cloned().collect();
-    let odds: Vec<Complex<T>> = data.iter().skip(1).step_by(2).cloned().collect();
+    let evens = data.iter().step_by(2).cloned().collect::<Vec<_>>();
+    let odds = data.iter().skip(1).step_by(2).cloned().collect::<Vec<_>>();
 
     let evens_res = fft_helper(&evens, sign);
     let odds_res = fft_helper(&odds, sign);
 
-    let mut res: Vec<Complex<T>> = vec![Complex::default(); n];
+    let mut res = vec![Complex64::default(); n];
     for t in 0..(n / 2) {
-        let val1: T = (sign * t as i32).into();
-        let val2: T = (n as i32).into();
-        let val3: T = TAU.into();
-        let xd: Complex<T> = Complex::new(T::zero(), (val1 / val2) * val3).exp();
+        let w = (sign * t as i32) as f64 / n as f64 * TAU;
+        let xd = Complex64::new(0.0, w).exp();
 
         res[t] = evens_res[t] + xd * odds_res[t];
         res[t + n / 2] = evens_res[t] - xd * odds_res[t];
@@ -37,45 +31,73 @@ where
 }
 
 fn fft(data: &Vec<Complex64>) -> Vec<Complex64> {
-    return fft_helper(data, -1);
-    //.iter()
-    //.map(|x| x / data.len() as f64)
-    //.collect();
+    return fft_helper(data, -1)
+        .iter()
+        .map(|x| x / data.len() as f64)
+        .collect();
 }
 
 fn ifft(data: &Vec<Complex64>) -> Vec<Complex64> {
     return fft_helper(data, 1);
 }
 
-fn main() {
-    {
-        let spec = hound::WavSpec {
-            channels: 1,
-            sample_rate: 44100,
-            bits_per_sample: 16,
-            sample_format: hound::SampleFormat::Int,
-        };
-        let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
-        for t in (0..44100).map(|x| x as f64 / 44100.0) {
-            let sample =
-                0.5 * (t * 3400.0 * 5.0 / 4.0 * TAU).sin() + 0.7 * (t * 3400.0 * TAU).sin();
-            let amplitude = i16::MAX as f64;
-            writer.write_sample((sample * amplitude) as i16).unwrap();
+fn write_sample() {
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+
+    let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
+    let amplitude = i16::MAX as f64;
+    for t in (0..44100).map(|x| x as f64) {
+        let sample = 0.3 * (t * 261.6256 / 44100.0 * TAU).sin();
+
+        if sample > 1.0 {
+            panic!("Sample is cutting off!");
         }
-        for t in (0..44100).map(|x| x as f64 / 44100.0) {
-            let sample =
-                0.5 * (t * 1000.0 * 2.0 * TAU).sin() + 0.7 * (t * 1000.0 * TAU).sin();
-            let amplitude = i16::MAX as f64;
-            writer.write_sample((sample * amplitude) as i16).unwrap();
-        }
+        writer.write_sample((sample * amplitude) as i16).unwrap();
     }
+    for t in (0..44100).map(|x| x as f64) {
+        let sample = 0.3 * (t * 329.6276 / 44100.0 * TAU).sin();
 
-    let data: Vec<Complex64> = vec![1.0.into(), 10.0.into(), 100.0.into(), 1000.0.into()];
+        if sample > 1.0 {
+            panic!("Sample is cutting off!");
+        }
+        writer.write_sample((sample * amplitude) as i16).unwrap();
+    }
+    for t in (0..44100).map(|x| x as f64) {
+        let sample = 0.3 * (t * 391.9954 / 44100.0 * TAU).sin();
 
-    println!("{:?}", data);
-    let bruh = fft(&data);
-    println!("{:?}", bruh);
-    println!("{:?}", ifft(&bruh));
+        if sample > 1.0 {
+            panic!("Sample is cutting off!");
+        }
+        writer.write_sample((sample * amplitude) as i16).unwrap();
+    }
+    for t in (0..(44100 * 3)).map(|x| x as f64) {
+        let sample = 0.3 * (t * 261.6256 / 44100.0 * TAU).sin()
+            + 0.3 * (t * 329.6276 / 44100.0 * TAU).sin()
+            + 0.3 * (t * 391.9954 / 44100.0 * TAU).sin();
+
+        if sample > 1.0 {
+            panic!("Sample is cutting off!");
+        }
+        writer.write_sample((sample * amplitude) as i16).unwrap();
+    }
+}
+
+fn main() {
+    write_sample();
+
+    {
+        let data: Vec<Complex64> = vec![1.0.into(), 10.0.into(), 100.0.into(), 1000.0.into()];
+
+        println!("{:?}", data);
+        let bruh = fft(&data);
+        println!("{:?}", bruh);
+        println!("{:?}", ifft(&bruh));
+    }
 
     //let mut reader = hound::WavReader::open("samples/120_G#_Leader_01_53_SP.wav").unwrap();
     let mut reader = hound::WavReader::open("sine.wav").unwrap();
@@ -84,37 +106,74 @@ fn main() {
 
     let channels = reader.spec().channels as usize;
     let bits = reader.spec().bits_per_sample;
-    let max_val = (2 as u32).pow(bits as u32);
+    let max_val: i32 = 1 << (bits - 1);
 
-    let all_samples: Vec<f64> = reader
+    let all_int_samples: Vec<_> = reader
         .samples::<i32>()
         .step_by(channels)
         .map(|x| x.unwrap())
-        .map(|x| x as f64 / max_val as f64)
         .collect();
+
+    let all_samples: Vec<f64> = all_int_samples
+        .iter()
+        .map(|x| x.clone() as f64 / max_val as f64)
+        .map(|x| if x > 1.0 { panic!("nooo") } else { x })
+        .collect();
+
+    let asd: i32 = all_int_samples.into_iter().max().unwrap();
+    println!("max_val: {}", max_val);
+    println!("Asd: {}", asd);
 
     let (mut rl, thread) = raylib::init().size(2048, 480).title("Hello, World").build();
 
+    let mut kernel_size: usize = 2048;
     let mut time: f64 = 0.0;
     while !rl.window_should_close() {
+        if rl.is_key_pressed(KeyboardKey::KEY_UP) {
+            kernel_size *= 2;
+        }
+        if kernel_size > 1 && rl.is_key_pressed(KeyboardKey::KEY_DOWN) {
+            kernel_size /= 2;
+        }
+
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::WHITE);
         time += d.get_frame_time() as f64;
         let curr_sample_start = (time * (reader.spec().sample_rate as f64)) as usize;
-        println!("{}", curr_sample_start);
+        println!("time={}, sample_start={}", time, curr_sample_start);
 
-        if curr_sample_start + 2048 < all_samples.len() {
-            let samples = &all_samples[curr_sample_start..(curr_sample_start + 2048)];
+        let w = d.get_screen_width();
+        let h = d.get_screen_height();
 
-            let data: Vec<Complex64> = samples.iter().map(|x| c64(x.clone(), 0.0)).collect();
-            let asd: Vec<Complex64> = fft(&data).iter().map(|x| x / data.len() as f64).collect();
+        if curr_sample_start + kernel_size < all_samples.len() {
+            let samples = &all_samples[curr_sample_start..(curr_sample_start + kernel_size)];
 
-            for i in 0..samples.len() {
+            let data: Vec<Complex64> = samples
+                .iter()
+                .map(|x| Into::<Complex64>::into(x.clone()))
+                .collect();
+            let res: Vec<Complex64> = fft(&data);
+
+            for x in 0..w {
+                let prog = x as f32 / w as f32;
+
+                let rot_per_sec = lerp(0.0, 1200.0, prog);
+                let rot_per_sample = rot_per_sec / 44100.0;
+                let fract_rot_per_sample = rot_per_sample * kernel_size as f32;
+                let i = fract_rot_per_sample as i32;
+                if i < 0 {
+                    continue;
+                }
+                let i = i as usize;
+                if i >= kernel_size {
+                    continue;
+                }
+
                 d.draw_line(
-                    i as i32,
+                    x as i32,
                     0,
-                    i as i32,
-                    (asd[i].abs() * 480.0 * 50.0) as i32,
+                    x as i32,
+                    (res[i].abs() * 3.0 * h as f64) as i32,
                     Color::BLACK,
                 );
             }
