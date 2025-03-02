4443d1182dbebbe0758c92cf6de2a4841c9ae11b
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-03-02 16:26:15 +0100

    calculate phase

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::f64::consts::TAU;
 
-use num_traits::Zero;
 use raylib::prelude::*;
 
 use num_complex::{Complex64, ComplexFloat};
@@ -54,53 +53,35 @@ fn write_sample() {
     let amplitude = i16::MAX as f64;
     for t in (0..44100).map(|x| x as f64) {
         let sample = 1.0 * (t * 261.6256 / 44100.0 * TAU).sin();
-
-        if sample > 1.0 {
-            panic!("Sample is cutting off!");
-        }
+        assert!(sample.abs() <= 1.0, "Sample is cutting off!");
         writer.write_sample((sample * amplitude) as i16).unwrap();
     }
     for t in (0..44100).map(|x| x as f64) {
         let sample = 1.0 * (t * 329.6276 / 44100.0 * TAU).sin();
-
-        if sample > 1.0 {
-            panic!("Sample is cutting off!");
-        }
+        assert!(sample.abs() <= 1.0, "Sample is cutting off!");
         writer.write_sample((sample * amplitude) as i16).unwrap();
     }
     for t in (0..44100).map(|x| x as f64) {
         let sample = 1.0 * (t * 391.9954 / 44100.0 * TAU).sin();
-
-        if sample > 1.0 {
-            panic!("Sample is cutting off!");
-        }
+        assert!(sample.abs() <= 1.0, "Sample is cutting off!");
         writer.write_sample((sample * amplitude) as i16).unwrap();
     }
     for t in (0..44100).map(|x| x as f64) {
         let sample = 1.0 * (t * 261.6256 / 44100.0 * TAU).sin();
-
-        if sample > 1.0 {
-            panic!("Sample is cutting off!");
-        }
+        assert!(sample.abs() <= 1.0, "Sample is cutting off!");
         writer.write_sample((sample * amplitude) as i16).unwrap();
     }
     for t in (0..44100).map(|x| x as f64) {
         let sample =
             0.5 * (t * 261.6256 / 44100.0 * TAU).sin() + 0.5 * (t * 329.6276 / 44100.0 * TAU).sin();
-
-        if sample > 1.0 {
-            panic!("Sample is cutting off!");
-        }
+        assert!(sample.abs() <= 1.0, "Sample is cutting off!");
         writer.write_sample((sample * amplitude) as i16).unwrap();
     }
     for t in (0..(44100 * 3)).map(|x| x as f64) {
         let sample = 0.33 * (t * 261.6256 / 44100.0 * TAU).sin()
             + 0.33 * (t * 329.6276 / 44100.0 * TAU).sin()
             + 0.33 * (t * 391.9954 / 44100.0 * TAU).sin();
-
-        if sample > 1.0 {
-            panic!("Sample is cutting off!");
-        }
+        assert!(sample.abs() <= 1.0, "Sample is cutting off!");
         writer.write_sample((sample * amplitude) as i16).unwrap();
     }
 }
@@ -143,70 +124,97 @@ fn main() {
     println!("max_val: {}", max_val);
     println!("Asd: {}", asd);
 
+    let samples_per_sec = reader.spec().sample_rate;
+
     let (mut rl, thread) = raylib::init().size(2000, 700).title("Hello, World").build();
 
     let mut kernel_size: usize = 2048;
     let mut time: f64 = 0.0;
+
+    let mut center = 600.0;
+    let mut range = 1200.0;
+
     while !rl.window_should_close() {
-        if rl.is_key_pressed(KeyboardKey::KEY_UP) {
+        let dt = rl.get_frame_time();
+        time += dt as f64;
+
+        let mut curr_sample_start = (time * (reader.spec().sample_rate as f64)) as usize;
+        println!("sample_start={}", curr_sample_start);
+
+        if rl.is_key_pressed(KeyboardKey::KEY_KP_ADD) {
             kernel_size *= 2;
         }
-        if kernel_size > 1 && rl.is_key_pressed(KeyboardKey::KEY_DOWN) {
+        if kernel_size > 1 && rl.is_key_pressed(KeyboardKey::KEY_KP_SUBTRACT) {
             kernel_size /= 2;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_UP) {
+            range *= (2 as f32).powf(dt);
+        }
+        if rl.is_key_down(KeyboardKey::KEY_DOWN) {
+            range /= (2 as f32).powf(dt);
+        }
+        if rl.is_key_down(KeyboardKey::KEY_RIGHT) {
+            center += range * 0.5 * dt;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_LEFT) {
+            center -= range * 0.5 * dt;
         }
 
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::WHITE);
-        time += d.get_frame_time() as f64;
-        let curr_sample_start = (time * (reader.spec().sample_rate as f64)) as usize;
-        println!("sample_start={}", curr_sample_start);
 
         let w = d.get_screen_width();
         let h = d.get_screen_height();
 
-        if curr_sample_start + kernel_size < all_samples.len() {
-            let samples = &all_samples[curr_sample_start..(curr_sample_start + kernel_size)];
+        if curr_sample_start + kernel_size >= all_samples.len() {
+            time = 0.0;
+            curr_sample_start = 0;
+        }
 
-            let data: Vec<Complex64> = samples
-                .iter()
-                .map(|x| Into::<Complex64>::into(x.clone()))
-                .collect();
-            let res: Vec<Complex64> = fft(&data);
+        let samples = &all_samples[curr_sample_start..(curr_sample_start + kernel_size)];
 
-            let mut freqs: Vec<Complex64> = vec![Complex64::zero(); kernel_size / 2 + 1];
-            freqs[0] = res[0];
-            freqs[kernel_size / 2] = res[kernel_size / 2];
-            for i in 1..(kernel_size / 2) {
-                freqs[i] = res[i] * 2.0
+        let data: Vec<Complex64> = samples
+            .iter()
+            .map(|x| Into::<Complex64>::into(x.clone()))
+            .collect();
+        let spinner_freqs: Vec<Complex64> = fft(&data);
+
+        let mut real_freqs: Vec<(f64, f64)> = vec![(0.0, 0.0); kernel_size / 2 + 1];
+        real_freqs[0] = (spinner_freqs[0].re(), 0.0);
+        assert!(spinner_freqs[0].im().abs() < 0.001);
+        real_freqs[kernel_size / 2] = (spinner_freqs[kernel_size / 2].re(), 0.0);
+        assert!(spinner_freqs[kernel_size / 2].im().abs() < 0.001);
+        for i in 1..(kernel_size / 2) {
+            // (a+bi)*exp(wit)+(a-bi)*exp(-wit) = 2a*cos(wt)-2b*cos(wt) = 2*sqrt(a^2+b^2)*cos(wt+atan2(b,a))
+            real_freqs[i] = (2.0 * spinner_freqs[i].abs(), spinner_freqs[i].arg());
+        }
+
+        let num_freqs = kernel_size / 2 + 1;
+
+        for x in 0..w {
+            let prog = x as f32 / w as f32;
+
+            let rot_per_sec = lerp(center - range / 2.0, center + range / 2.0, prog);
+            let rot_per_sample = rot_per_sec / samples_per_sec as f32;
+            let fract_rot_per_sample = rot_per_sample * kernel_size as f32;
+            let i = fract_rot_per_sample.floor() as i32;
+            if i < 0 {
+                d.draw_line(x as i32, 0, x as i32, h, Color::RED);
+                continue;
+            }
+            let i = i as usize;
+            if i >= num_freqs {
+                d.draw_line(x as i32, 0, x as i32, h, Color::RED);
+                continue;
             }
 
-            let num_freqs = kernel_size / 2 + 1;
-
-            for x in 0..w {
-                let prog = x as f32 / w as f32;
-
-                let rot_per_sec = lerp(-100.0, 3000.0, prog);
-                let rot_per_sample = rot_per_sec / 44100.0;
-                let fract_rot_per_sample = rot_per_sample * kernel_size as f32;
-                let i = fract_rot_per_sample.floor() as i32;
-                if i < 0 {
-                    d.draw_line(x as i32, 0, x as i32, h, Color::RED);
-                    continue;
-                }
-                let i = i as usize;
-                if i >= num_freqs {
-                    d.draw_line(x as i32, 0, x as i32, h, Color::RED);
-                    continue;
-                }
-
-                d.draw_line(
-                    x as i32,
-                    0,
-                    x as i32,
-                    (freqs[i].abs() * h as f64) as i32,
-                    Color::BLACK,
-                );
-            }
+            d.draw_line(
+                x as i32,
+                0,
+                x as i32,
+                (real_freqs[i].0 * h as f64) as i32,
+                Color::BLACK,
+            );
         }
     }
 }
