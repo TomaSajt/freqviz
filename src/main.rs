a91211bfb0cfb0b698ed043c6e792de4113b0006
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-04-09 14:10:51 +0200

    refactor stuff

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 use std::{
-    collections::{vec_deque, VecDeque},
-    ops::Rem,
+    collections::VecDeque,
     sync::{Arc, Mutex},
-    thread,
-    time::Duration,
 };
 
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-
-mod record_wav;
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    Stream,
+};
 
 use std::f64::consts::TAU;
 
 use raylib::prelude::*;
 
 use num_complex::{Complex64, ComplexFloat};
+
+struct AppState {
+    time: f64,
+    kernel_size: usize,
+    center: f64,
+    range: f64,
+    height_scale: f64,
+    deque_mutex: Arc<Mutex<VecDeque<f64>>>,
+    audio_stream: Stream,
+    samples_per_sec: u32,
+}
 
 fn fft_helper(data: &Vec<Complex64>, sign: i32) -> Vec<Complex64> {
     let n = data.len();
@@ -51,6 +60,19 @@ pub fn fft(data: &Vec<Complex64>) -> Vec<Complex64> {
 
 pub fn ifft(data: &Vec<Complex64>) -> Vec<Complex64> {
     return fft_helper(data, 1);
+}
+
+fn is_almost_same(a: Complex64, b: Complex64) -> bool {
+    (a - b).abs() < 0.00000001
+}
+
+#[test]
+fn test_fft() {
+    let a: Vec<Complex64> = vec![1.0.into(), 10.0.into(), 100.0.into(), 1000.0.into()];
+    let b = fft(&a);
+    let c = ifft(&b);
+    let d = a.into_iter().zip(c).all(|x| is_almost_same(x.0, x.1));
+    assert!(d);
 }
 
 fn write_sample() {
@@ -98,49 +120,39 @@ fn write_sample() {
     }
 }
 
-pub fn do_the_thing() {}
+fn update_state(rl: &mut RaylibHandle, state: &mut AppState) {
+    let dt = rl.get_frame_time() as f64;
+    state.time += dt;
 
-fn main() -> Result<(), anyhow::Error> {
-    println!("HELLO WORLD");
-
-    {
-        {
-            let data: Vec<Complex64> = vec![1.0.into(), 10.0.into(), 100.0.into(), 1000.0.into()];
-
-            println!("{:?}", data);
-            let bruh = fft(&data);
-            println!("{:?}", bruh);
-            println!("{:?}", ifft(&bruh));
-        }
-
-        //let mut reader = hound::WavReader::open("samples/120_G#_Leader_01_53_SP.wav").unwrap();
-        //let mut reader = hound::WavReader::open("samples/124_Fs_Upright_408_SP_01.wav").unwrap();
-        //let mut reader = hound::WavReader::open("Beethoven_Sonate_n14_1er_mvt.wav").unwrap();
-        let mut reader = hound::WavReader::open("sine.wav").unwrap();
-
-        println!("{:?}", reader.spec());
-
-        let channels = reader.spec().channels as usize;
-        let bits = reader.spec().bits_per_sample;
-        let max_val: i32 = 1 << (bits - 1);
-
-        let all_int_samples: Vec<_> = reader
-            .samples::<i32>()
-            .step_by(channels)
-            .map(|x| x.unwrap())
-            .collect();
-
-        let all_samples: Vec<f64> = all_int_samples
-            .iter()
-            .map(|x| x.clone() as f64 / max_val as f64)
-            .map(|x| if x > 1.0 { panic!("nooo") } else { x })
-            .collect();
-
-        let asd: i32 = all_int_samples.into_iter().max().unwrap();
-        println!("max_val: {}", max_val);
-        println!("Asd: {}", asd);
+    if rl.is_key_pressed(KeyboardKey::KEY_D) {
+        state.kernel_size *= 2;
     }
+    if state.kernel_size > 1 && rl.is_key_pressed(KeyboardKey::KEY_A) {
+        state.kernel_size /= 2;
+    }
+    if rl.is_key_down(KeyboardKey::KEY_UP) {
+        state.range *= 2.0.powf(dt);
+    }
+    if rl.is_key_down(KeyboardKey::KEY_DOWN) {
+        state.range /= 2.0.powf(dt);
+    }
+    if rl.is_key_down(KeyboardKey::KEY_W) {
+        state.height_scale *= 5.0.powf(dt);
+    }
+    if rl.is_key_down(KeyboardKey::KEY_S) {
+        state.height_scale /= 5.0.powf(dt);
+    }
+    if rl.is_key_down(KeyboardKey::KEY_RIGHT) {
+        state.center += state.range * 0.5 * dt;
+    }
+    if rl.is_key_down(KeyboardKey::KEY_LEFT) {
+        state.center -= state.range * 0.5 * dt;
+    }
+}
 
+fn make_audio_stream(
+    deque_mutex: Arc<Mutex<VecDeque<f64>>>,
+) -> Result<(Stream, u32), anyhow::Error> {
     let host = cpal::default_host();
 
     // Set up the input device and stream with the default input config.
@@ -158,37 +170,21 @@ fn main() -> Result<(), anyhow::Error> {
 
     println!("Default input config: {:?}", config);
 
-    // A flag to indicate that recording is in progress.
-    println!("Begin recording...");
-
     assert_eq!(config.config().channels, 2);
-
-    // Run the input stream on a separate thread.
-    let err_fn = move |err| {
-        eprintln!("an error occurred on stream: {}", err);
-    };
-
-    let channel_count = config.config().channels as usize;
-
-    let deque_mutex = Arc::new(Mutex::new(VecDeque::<f64>::new()));
-
-    let deque_mutex_2 = deque_mutex.clone();
 
     let stream = match config.sample_format() {
         cpal::SampleFormat::F32 => device.build_input_stream(
             &config.into(),
             move |data: &[f32], _: &_| {
-                assert_eq!(data.len().rem(channel_count), 0);
-                let mut deque = deque_mutex_2.lock().unwrap();
-                deque.push_back(1.0);
-                //println!("{:?}", deque.len());
-                //println!("data len {:?}", data.len());
-                for sample in data {
+                let mut deque = deque_mutex.lock().unwrap();
+                // TODO: figure out if we need to skip per channel
+                for sample in data.iter().step_by(2) {
                     deque.push_back(sample.clone() as f64);
                 }
-                //println!("deque len {:?}", deque.len());
             },
-            err_fn,
+            |err| {
+                eprintln!("an error occurred on stream: {}", err);
+            },
             None,
         )?,
         sample_format => {
@@ -198,72 +194,71 @@ fn main() -> Result<(), anyhow::Error> {
         }
     };
 
-    stream.play()?;
+    Ok((stream, samples_per_sec))
+}
 
-    let (mut rl, thread) = raylib::init().size(2000, 700).title("Hello, World").build();
+fn range_transform(l1: f64, r1: f64, l2: f64, r2: f64, val: f64) -> f64 {
+    (r2 - l2) / (r1 - l1) * (val - l1) + l2
+}
 
-    let mut kernel_size: usize = 2048;
-    let mut time: f64 = 0.0;
+fn main() -> Result<(), anyhow::Error> {
+    // write_sample();
 
-    let mut center = 600.0;
-    let mut range = 1200.0;
+    let deque_mutex = Arc::new(Mutex::new(VecDeque::<f64>::new()));
+    let (audio_stream, samples_per_sec) = make_audio_stream(deque_mutex.clone())?;
+    audio_stream.play()?;
+
+    let mut state = AppState {
+        time: 0.0,
+
+        kernel_size: 2048,
+
+        center: 600.0,
+        range: 1200.0,
+        height_scale: 1.0,
+
+        deque_mutex,
+        audio_stream,
+        samples_per_sec,
+    };
+
+    let (mut rl, thread) = raylib::init().size(2000, 700).title("FreqViz").build();
 
     while !rl.window_should_close() {
-        let dt = rl.get_frame_time();
-        time += dt as f64;
+        update_state(&mut rl, &mut state);
 
-        let curr_sample_start = (time * (samples_per_sec as f64)) as usize;
-        println!("sample_start={}", curr_sample_start);
+        let curr_sample_start = (state.time * (samples_per_sec as f64)) as usize;
+        println!("curr_sample_start={curr_sample_start}");
 
-        if rl.is_key_pressed(KeyboardKey::KEY_KP_ADD) {
-            kernel_size *= 2;
-        }
-        if kernel_size > 1 && rl.is_key_pressed(KeyboardKey::KEY_KP_SUBTRACT) {
-            kernel_size /= 2;
-        }
-        if rl.is_key_down(KeyboardKey::KEY_UP) {
-            range *= (2 as f32).powf(dt);
-        }
-        if rl.is_key_down(KeyboardKey::KEY_DOWN) {
-            range /= (2 as f32).powf(dt);
-        }
-        if rl.is_key_down(KeyboardKey::KEY_RIGHT) {
-            center += range * 0.5 * dt;
-        }
-        if rl.is_key_down(KeyboardKey::KEY_LEFT) {
-            center -= range * 0.5 * dt;
-        }
+        let kernel_size = state.kernel_size;
 
-        let mut d = rl.begin_drawing(&thread);
-        d.clear_background(Color::WHITE);
-
-        let w = d.get_screen_width();
-        let h = d.get_screen_height();
-
-        let data;
-        let mut deque = deque_mutex.lock().unwrap();
-        println!("{}", deque.len());
-        while deque.len() > kernel_size {
-            deque.pop_front();
-        }
-        if deque.len() < kernel_size {
-            continue;
-        }
-        data = deque
-            .iter()
-            .map(|x| Into::<Complex64>::into(x.clone()))
-            .collect();
-        drop(deque);
+        let data = {
+            let mut deque = state.deque_mutex.lock().unwrap();
+            println!("{}", deque.len());
+            // only keep as much data in the deque as we need
+            while deque.len() > state.kernel_size {
+                deque.pop_front();
+            }
+            // pad the data with zeroes if not enough data
+            while deque.len() < state.kernel_size {
+                deque.push_front(0.0);
+            }
+            deque
+                .iter()
+                .map(|x| Into::<Complex64>::into(x.clone()))
+                .collect::<Vec<_>>()
+        };
 
         let spinner_freqs: Vec<Complex64> = fft(&data);
 
-        let mut real_freqs: Vec<(f64, f64)> = vec![(0.0, 0.0); kernel_size / 2 + 1];
+        let mut real_freqs: Vec<(f64, f64)> = vec![(0.0, 0.0); state.kernel_size / 2 + 1];
 
         // we know that spinner_freqs[i]*exp(i/n*tau*t)+spinner_freqs[n-i]*exp(-i/n*tau*t) has to sum to a real number
         // this way we know that = spinner_freqs[i] is the complex conjugate of spinner_freqs[n-i]
 
         // we don't want negative amplitudes
         // if it happens, we just store a half-turn phaseshift instead
+
         real_freqs[0] = (spinner_freqs[0].abs(), spinner_freqs[0].arg());
         assert!(spinner_freqs[0].im().abs() < 0.001);
 
@@ -280,12 +275,39 @@ fn main() -> Result<(), anyhow::Error> {
 
         let num_freqs = kernel_size / 2 + 1;
 
-        for x in 0..w {
-            let prog = x as f32 / w as f32;
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::WHITE);
+        d.draw_text("Hello", 10, 10, 20, Color::BISQUE);
 
-            let rot_per_sec = lerp(center - range / 2.0, center + range / 2.0, prog);
-            let rot_per_sample = rot_per_sec / samples_per_sec as f32;
-            let fract_rot_per_sample = rot_per_sample * kernel_size as f32;
+        let w = d.get_screen_width();
+        let h = d.get_screen_height();
+
+        for freq in [110.0, 220.0, 440.0, 880.0, 1760.0] {
+            let x = range_transform(
+                state.center - state.range / 2.0,
+                state.center + state.range / 2.0,
+                0.0,
+                w as f64,
+                freq,
+            ) as i32;
+
+            d.draw_line(x, 0, x, h, Color::BLUE);
+            let text = freq.to_string() + "Hz";
+            let font_size = 20;
+            d.draw_text(
+                &text,
+                x - d.measure_text(&text, font_size) / 2,
+                h - font_size - 10,
+                font_size,
+                Color::BLUEVIOLET,
+            );
+        }
+
+        for x in 0..w {
+            let prog = x as f64 / w as f64;
+            let rot_per_sec = (state.center - state.range / 2.0) + state.range * prog;
+            let rot_per_sample = rot_per_sec / samples_per_sec as f64;
+            let fract_rot_per_sample = rot_per_sample * kernel_size as f64;
             let i = fract_rot_per_sample.floor() as i32;
             if i < 0 {
                 d.draw_line(x as i32, 0, x as i32, h, Color::RED);
@@ -301,7 +323,7 @@ fn main() -> Result<(), anyhow::Error> {
                 x as i32,
                 0,
                 x as i32,
-                (real_freqs[i].0 * h as f64) as i32,
+                (real_freqs[i].0 * h as f64 * state.height_scale) as i32,
                 Color::BLACK,
             );
         }
